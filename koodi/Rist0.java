5c1d86c1fa27eed48dc03b2d50db1223e91092c6
mshroom <32199029+mshroom@users.noreply.github.com>
2018-01-08 22:50:52 +0200

    Update Rist0.java

--- koodi/Rist0.java
+++ koodi/Rist0.java
@@ -1,36 +1,38 @@
 import lejos.nxt.*;
-import lejos.util.Delay;
-import java.util.Random;
 
 /**
- * Pääluokka tällä hetkellä testaa robotin käsivarren ja valosensorin toimintaa. Robotti kulkee peliruudukossa ja pudottaa pelimerkin jokaiseen ruutuun, jossa ei ole vastustajan pelimerkkiä.
+ * Pääluokka suorittaa yhden pelikerran alusta loppuun. Ihminen saa valita kumpi aloittaa.
  * 
  * @author mshroom
  * 
  */
 public class Rist0 {
 	public static void main (String[] aArg) throws Exception  {
-		Pelilauta peli = new Pelilauta();
-		LightSensor light = new LightSensor(SensorPort.S1);
-		
-	// Kalibroidaan valosensori mustassa ja valkoisessa ruudussa
-	light.calibrateLow();
-	peli.kuljeRuutuun(2, 2);
-	Delay.msDelay(1000);
-	light.calibrateHigh();
-	Delay.msDelay(1000);
-	
-		
-	for (int x = 0; x < 3; x ++) {
-		for (int y = 0; y < 3; y ++) {
-			peli.kuljeRuutuun(x, y);
-			Delay.msDelay(1000);
-			if (light.getLightValue() > 50) {
-				peli.jataPelimerkki();
-			}
-			Delay.msDelay(2000);
-		}
-	}
-	peli.palaaKotiin();
-	}
+	Peli peli = new Peli();
+	  peli.kalibroiPelilauta();
+	  while (true) {
+		  LCD.drawString("Kumpi aloittaa?", 0, 0);
+		  LCD.drawString("< Robotti", 0, 2);
+		  LCD.drawString("> Ihminen", 0, 4);
+		  if (Button.LEFT.isPressed()) {
+			  LCD.clear();
+			  peli.siirto();
+			  break;
+		  }
+		  if (Button.RIGHT.isPressed()) {
+			  LCD.clear();
+			  break;
+		  }  
+	  }
+	  
+	  while (!peli.peliOhi()) {
+		  Delay.msDelay(5000);
+		  if (peli.paivitaPelitilanne()) {
+			  if (!peli.peliOhi()) {
+				  peli.siirto();
+			  }			  
+		  } else {
+			  peli.ilmoitaVirheesta();
+		  }
+	  }
 }
