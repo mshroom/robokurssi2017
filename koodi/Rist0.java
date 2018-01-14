45beb6a2c4dbf5ba9d97b9543f758997a253e30c
mshroom <32199029+mshroom@users.noreply.github.com>
2018-01-14 16:32:18 +0200

    Update Rist0.java

--- koodi/Rist0.java
+++ koodi/Rist0.java
@@ -1,15 +1,37 @@
 import lejos.nxt.*;
+import lejos.util.Delay;
 
 /**
- * Pääluokka suorittaa yhden pelikerran alusta loppuun. Ihminen saa valita kumpi aloittaa.
+ * Pääluokka suorittaa yhden pelikerran alusta loppuun. Ihminen saa valita vaikeustason ja aloittavan pelaajan.
  * 
  * @author mshroom
  * 
  */
 public class Rist0 {
-	public static void main (String[] aArg) throws Exception  {
-	Peli peli = new Peli();
+  public static void main (String[] aArg) throws Exception  {
+	  
+	  int vaikeustaso = 0;
+	  while (true) {
+		  LCD.drawString("Vaikeustaso", 0, 0);
+		  LCD.drawString("< Helppo", 0, 2);
+		  LCD.drawString("> Vaikea", 0, 4);
+		  if (Button.LEFT.isPressed()) {
+			  LCD.clear();
+			  vaikeustaso = 1;
+			  break;
+		  }
+		  if (Button.RIGHT.isPressed()) {
+			  LCD.clear();
+			  vaikeustaso = 2;
+			  break;
+		  }
+	  }
+	  
+	  LCD.drawString("Kalibroidaan", 0, 0);
+	  Peli peli = new Peli(vaikeustaso);
 	  peli.kalibroiPelilauta();
+	  LCD.clear();
+	  
 	  while (true) {
 		  LCD.drawString("Kumpi aloittaa?", 0, 0);
 		  LCD.drawString("< Robotti", 0, 2);
@@ -26,6 +48,8 @@ public class Rist0 {
 	  }
 	  
 	  while (!peli.peliOhi()) {
+		  LCD.clear();
+		  LCD.drawString("Sinun vuorosi", 0, 0);
 		  Delay.msDelay(5000);
 		  if (peli.paivitaPelitilanne()) {
 			  if (!peli.peliOhi()) {
@@ -35,4 +59,7 @@ public class Rist0 {
 			  peli.ilmoitaVirheesta();
 		  }
 	  }
+	  	  
+	  peli.kerroTulos();
+  }
 }
